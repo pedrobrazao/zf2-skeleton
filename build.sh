218d0e3ecb783a242d01b9c3a5347ceea3fa252c
Pedro Ferreira <pedro@maido.com>
2014-11-27 14:18:30 +0000

    Removed minified javascripts

--- build.sh
+++ build.sh
@@ -10,4 +10,7 @@ compass compile
 # compress css and javascript files
 yui-compressor public/assets/css/styles.css > public/assets/css/styles.min.css
 yui-compressor public/assets/js/bootstrap.js > public/assets/js/bootstrap.min.js
+yui-compressor public/assets/js/jquery.js > public/assets/js/jquery.min.js
+yui-compressor public/assets/js/html5shiv.js > public/assets/js/html5shiv.min.js
+yui-compressor public/assets/js/respond.js > public/assets/js/respond.min.js
 
